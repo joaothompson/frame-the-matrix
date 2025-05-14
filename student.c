341101a04738777e6965f2cf265b1a7954c14aaa
joaothompson <37004063+joaothompson@users.noreply.github.com>
2025-05-13 23:28:21 -0300

    Update student.c

--- student.c
+++ student.c
@@ -1,7 +1,34 @@
 #include "student.h"
 
+void readMatrix(int m, int n, int matrix[m][n])
+{
+  return;
+}
+
+void readInteger(int *var)
+{
+  scanf("%d", var);
+  return;
+}
+
+void printMatrix(int m, int n, int matrix[m][n])
+{
+  return;
+}
+
+void frameMatrix(int m, int n, int matrix[m][n], int framed[m+2][n+2])
+{
+  return;
+}
+
 void student(void) {
   // TODO: do everything.
-  
+  int m, n, std;
+  readInteger();
+  readInteger();
+  readMatrix();
+  readInteger();
+  frameMatrix();
+  printMatrix();
   return;
 }
